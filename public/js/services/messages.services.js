7ef7da45533e688ccf848a52e68248bee1ccdf71
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-27 13:41:50 -1000

    messages.services ok for now

--- public/js/services/messages.services.js
+++ public/js/services/messages.services.js
@@ -1,1 +1,54 @@
-messages.services.js
+//services ALWAYS return a FUNCTION
+//services are a FUNCTION
+//services handles your models and data, and talks to the back-end (server)
+
+angular
+.module('myApp')
+.service('MessagesService', ['$http', function($http) {
+  var url = '/api/messages';
+  var self = this;
+
+  //collection of messages
+  this.messages =[];
+
+  //initialization
+  $http.get(url)
+  .then(function(response) {
+    self.users = response.data;
+  });
+
+  //get methods
+  this.getMessages = function() {
+    return messages;
+  };
+  this.getMessage = function(index) {
+    return messages[index];
+  };
+
+  //create method
+  this.addMessage = function(givenMessage) {
+    if(!givenMessage) {
+      return;
+    }
+    //create on front-end
+    var message = {
+      body: givenMessage.body,
+    };
+
+    //created on back-end
+    return $http.post(url, message)
+    .then(function(response){
+      self.messages.push(response.data);
+      console.log('response data', response.data);
+      console.log('message created on back-end');
+      return response.data;
+    });
+  };
+
+  //to update the url
+  this.update = function(id, user) {
+    var updateUrl = url + '/' + id;
+    $http.put(updateUrl, message);
+  };
+
+}]);
