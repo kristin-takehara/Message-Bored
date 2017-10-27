34b6b3d361c7fd076257b1381b76a731c988624e
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-27 13:39:47 -1000

    message controller set

--- public/js/controllers/messages.controller.js
+++ public/js/controllers/messages.controller.js
@@ -1,1 +1,21 @@
-messages.controller.js
+//only need to dependency inject its service(MessagesService)
+//therefore: $scope.messages = MessagesService.messages
+
+angular
+.module('myApp')
+.controller('MessagesController', ['$scope', 'MessagesService', function($scope, MessagesService) {
+  $scope.messageSearch = '';
+  $scope.MessagesService = MessagesService;
+
+  // $scope.newMessage = {
+  //   body: ''
+  // };
+
+  $scope.addMessage = function(e) {
+    MessagesService.addMessage($scope.newMessage)
+    .then(function() {
+     $scope.newMessage.body = '';
+    });
+  };
+
+}]);
