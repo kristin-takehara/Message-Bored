0a2794f82127893ce2ac52e5f0e5602c483b4383
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-28 17:18:01 -1000

    styling added. need to re-check functionality

--- public/js/controllers/userLogin.controller.js
+++ public/js/controllers/userLogin.controller.js
@@ -1,15 +1,16 @@
 angular
 .module('myApp')
-.controller('UserLoginController', ['UsersServices', '$scope', '$routeParams', function(UsersServices, $scope, $routeParams) {
+.controller('UserLoginController', ['UsersServices', '$scope', '$routeParams', '$timeout', function(UsersServices, $scope, $routeParams, $timeout) {
 
   $scope.UsersServices = UsersServices;
 
   $scope.loginObj = UsersServices.getLoggedIn();//<<<--check
 
   $scope.logOut = UsersServices.logOut;
-  $scope.logoutHeader = "successfully logged out";
-  $timeout(function() {
-    $scope.logoutHeader = "..::Thank You For Coming::..";
-  }, 3000);
+
+  // $scope.logoutHeader = "successfully logged out";
+  // $timeout(function() {
+  //   $scope.logoutHeader = "..::Thank You For Coming::..";
+  // }, 3000);
 
 }]);
